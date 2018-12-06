0cdf7d7cf595076664b53d6ab9619e4606e457ce
Matt Casper <matthewvcasper@gmail.com>
2018-12-06 12:52:21 -0800

    Day 1 Part 1, Rust

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input_data: String = String::from_utf8(fs::read("input/part_1.txt").unwrap()).unwrap();
+    let inputs: Vec<&str> = input_data.trim().split('\n').collect();
+    let result = inputs.iter().fold(0, |acc, input| {
+        acc + input.parse::<i32>().unwrap()
+    });
+
+    println!("{}", result);
 }
