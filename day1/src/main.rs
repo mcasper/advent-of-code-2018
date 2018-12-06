8266481e6277e4674674350775a39c8678952b3c
Matt Casper <matthewvcasper@gmail.com>
2018-12-06 13:21:35 -0800

    Day 1 Part 2, Rust

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,29 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let input_data: String = String::from_utf8(fs::read("input/part_1.txt").unwrap()).unwrap();
     let inputs: Vec<&str> = input_data.trim().split('\n').collect();
-    let result = inputs.iter().fold(0, |acc, input| {
+    let part_1_result = inputs.iter().fold(0, |acc, input| {
         acc + input.parse::<i32>().unwrap()
     });
 
-    println!("{}", result);
+    let mut known_frequencies: HashMap<i32, i32> = HashMap::new();
+    let mut part_2_result = 0;
+    let mut solved = false;
+    let mut counter = 0;
+    while !solved {
+        for input in inputs.iter() {
+            counter = counter + input.parse::<i32>().unwrap();
+            if known_frequencies.contains_key(&counter) && !solved {
+                part_2_result = counter;
+                solved = true;
+            } else {
+                known_frequencies.insert(counter, 1);
+            }
+        }
+    }
+
+    println!("Part 1: {}", part_1_result);
+    println!("Part 2: {}", part_2_result);
 }
