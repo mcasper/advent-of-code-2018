105a5dd26276ef832e2c0f08cce20690d4dfdb48
Matt Casper <matthewvcasper@gmail.com>
2018-12-11 23:01:10 -0800

    Day 8, Part 2, Ruby

--- day8/day8.rb
+++ day8/day8.rb
@@ -8,16 +8,26 @@ class Node
     metadata.sum + nodes.map(&:metadata_sum).sum
   end
 
+  def value
+    if nodes.empty?
+      metadata.sum
+    else
+      metadata.map { |m| nodes[m-1]&.value }.compact.sum
+    end
+  end
+
   attr_reader :metadata, :nodes
 end
 
 def parse_node(children_count, metadata_count, lazy)
-  children = 0.upto(children_count-1).map { parse_node(lazy.next, lazy.next, lazy) }
-  metadata = 0.upto(metadata_count-1).map { lazy.next.to_i }
+  children = children_count.times.map { parse_node(lazy.next, lazy.next, lazy) }
+  metadata = metadata_count.times.map { lazy.next.to_i }
   Node.new(metadata: metadata, nodes: children)
 end
 
 numbers = File.read("input.txt").strip.split(" ").map(&:to_i)
 
 lazy = numbers.lazy
-puts parse_node(lazy.next, lazy.next, lazy).metadata_sum
+tree = parse_node(lazy.next, lazy.next, lazy)
+puts tree.metadata_sum
+puts tree.value
